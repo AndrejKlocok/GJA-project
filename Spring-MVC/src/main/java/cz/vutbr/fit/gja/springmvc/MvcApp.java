1cb66533d0f85b39163305a43356079e81d20151
Martin Kocour <martinkocour1102@gmail.com>
2018-12-28 16:02:10 +0100

    MVC

---
+++ Spring-MVC/src/main/java/cz/vutbr/fit/gja/springmvc/MvcApp.java
@@ -0,0 +1,16 @@
+package cz.vutbr.fit.gja.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Martin Kocour
+ * created on 23/12/2018
+ */
+@SpringBootApplication
+public class MvcApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcApp.class, args);
+    }
+}
